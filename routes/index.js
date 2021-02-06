b47d324c7de888c518d3f84c152118d230d1dc07
Shubham Singh <sngh2695@gmail.com>
2021-02-06 20:24:12 +0530

    Setup our first controller for home

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router=express.Router();
 
+//including controller
+const homeController=require('../controllers/home_controller');
+
 console.log("Router loaded");
+
+router.get('/',homeController.home);
 
 module.exports=router;
