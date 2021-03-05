6c928d892876254c33e1ae375e9eafa0e04b00ff
shubham singh <shubham.singh@truminds.com>
2021-03-05 17:15:37 +0530

    post controller created

--- models/post.js
+++ models/post.js
@@ -1,19 +1,19 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const postSchema=new Mongoose.Schema({
-    content:{
-        type:String,
-        required:true
+
+const postSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
     },
-    user:{
-        type :mongoose.Schema.Types.ObjectId,
-        // reference to the user schema , i.e one user can have multiple posts
-        ref:'User'
+    user: {
+        type:  mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+
     }
 },{
-    timestamps:true
+    timestamps: true
 });
 
-const Post=mongoose.model('Post',postSchema);
-module.exports=Post;
-
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
